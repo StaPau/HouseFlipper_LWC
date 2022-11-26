9ea8d4b116b5d6771fae354a879ef07bdbafbb4b
StaPau <paulina.stadryniak@gmail.com>
2022-11-26 10:03:22 +0100

    Edit for dev org deployment 2

--- force-app/main/default/lwc/hfSearchProductEngine/hfSearchProductEngine.js
+++ force-app/main/default/lwc/hfSearchProductEngine/hfSearchProductEngine.js
@@ -7,17 +7,17 @@ export default class HfSearchProductEngine extends LightningElement {
     isUserB2B = true;
 
 
-    get options(){
-        return[
-            {label: 'Yes', value='true'},
-            {label: 'No', value='false'},
-            ];
-    }
-    handleKeyUp(evt) {
-        const isEnterKey = evt.keyCode;
-        
-        if (isEnterKey == 13) {
-            this.queryTerm = evt.target.value;
-        }
-    }
+//    get options(){
+//        return[
+//            {label: 'Yes', value='true'},
+//            {label: 'No', value='false'},
+//            ];
+//    }
+//    handleKeyUp(evt) {
+//        const isEnterKey = evt.keyCode;
+//
+//        if (isEnterKey == 13) {
+//            this.queryTerm = evt.target.value;
+//        }
+//    }
 }
